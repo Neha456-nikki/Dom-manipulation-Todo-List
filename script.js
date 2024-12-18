4e24f04174e16e0d6fe77e621f0d2d2175dcb90a
Neha456-nikki <nnegi4343@gmail.com>
2024-12-18 16:38:10 +0530

    Started adding todos

---
+++ script.js
@@ -0,0 +1,42 @@
+function loadTodos() {
+    //This function will load the todos from the browser
+    const todos = JSON.parse(localStorage.getItem("todos")) || {"todoList": []};
+    console.log(todos);
+    return todos;
+}
+
+function addTodoToLocalStorage(todoText) {
+    const todos = loadTodos();
+    todos.todoList.push(todoText);
+    localStorage.setItem("todos", JSON.stringify(todos));
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+
+   const todoInput = document.getElementById("todoInput")
+
+   const submitButton = document.getElementById("addTodo");
+
+   submitButton.addEventListener("clicl", (event) => {
+      const todoText = todoInput.value;
+      if(todoText == '') {
+        alert("please write something for the todo");
+      } else {
+        addTodoToLocalStorage(todoText);
+
+      }
+
+   });
+
+    todoInput.addEventListener("change", (event) => {
+        //this call back method is fired everytime there is a change in the input tag
+       const todoText = event.target.value;
+
+       event.target.value = todoText.trim();
+
+       console.log(event.target.value)
+    })
+
+    loadTodos();
+});
